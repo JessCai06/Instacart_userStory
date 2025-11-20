d151446b8c8f89b499423944e0888619f609fcfb
Grace <grace.tianyuan@outlook.com>
2025-11-20 17:24:30 -0500

    sql files

---
+++ us7_create.sql
@@ -0,0 +1,82 @@
+-- Created by Redgate Data Modeler (https://datamodeler.redgate-platform.com)
+-- Last modification date: 2025-11-20 20:37:21.723
+
+
+
+-- tables
+-- Table: Batch
+CREATE TABLE Batch (
+    batch_id int  NOT NULL,
+    batch_status Batch_Status,
+    shopper_id int  NOT NULL,
+    CONSTRAINT Batch_pk PRIMARY KEY (batch_id)
+);
+
+-- Table: Order
+CREATE TABLE "Order" (
+    order_id int  NOT NULL,
+    tips money  NOT NULL,
+    order_fee money  NOT NULL,
+    order_status Order_Status,
+    store_id int  NOT NULL,
+    batch_id int  NOT NULL,
+    CONSTRAINT Order_pk PRIMARY KEY (order_id)
+);
+
+-- Table: Retail
+CREATE TABLE Retail (
+    retail_id int  NOT NULL,
+    name int  NOT NULL,
+    CONSTRAINT Retail_pk PRIMARY KEY (retail_id)
+);
+
+-- Table: Shopper
+CREATE TABLE Shopper (
+    shopper_id int  NOT NULL,
+    shopping_status Shopping_Status,
+    CONSTRAINT Shopper_pk PRIMARY KEY (shopper_id)
+);
+
+-- Table: Store
+CREATE TABLE Store (
+    store_id int  NOT NULL,
+    location text  NOT NULL,
+    retail_id int  NOT NULL,
+    CONSTRAINT Store_pk PRIMARY KEY (store_id)
+);
+
+-- foreign keys
+-- Reference: Batch_Shopper (table: Batch)
+ALTER TABLE Batch ADD CONSTRAINT Batch_Shopper
+    FOREIGN KEY (shopper_id)
+    REFERENCES Shopper (shopper_id)  
+    NOT DEFERRABLE 
+    INITIALLY IMMEDIATE
+;
+
+-- Reference: Order_Batch (table: Order)
+ALTER TABLE "Order" ADD CONSTRAINT Order_Batch
+    FOREIGN KEY (batch_id)
+    REFERENCES Batch (batch_id)  
+    NOT DEFERRABLE 
+    INITIALLY IMMEDIATE
+;
+
+-- Reference: Order_Store (table: Order)
+ALTER TABLE "Order" ADD CONSTRAINT Order_Store
+    FOREIGN KEY (store_id)
+    REFERENCES Store (store_id)  
+    NOT DEFERRABLE 
+    INITIALLY IMMEDIATE
+;
+
+-- Reference: Store_Retail (table: Store)
+ALTER TABLE Store ADD CONSTRAINT Store_Retail
+    FOREIGN KEY (retail_id)
+    REFERENCES Retail (retail_id)  
+    NOT DEFERRABLE 
+    INITIALLY IMMEDIATE
+;
+
+-- End of file.
+
---
+++ us7_initialize.sql
@@ -0,0 +1,27 @@
+-- drop the trax database if it exists
+DROP database if EXISTS instacart;
+
+SET datestyle = 'ISO, MDY';
+
+-- create it afresh
+CREATE database instacart;
+\c instacart
+
+
+DROP IF EXISTS Order_Status
+DROP IF EXISTS Batch_Status
+DROP IF EXISTS Shopping_Status
+
+CREATE TYPE Order_Status AS ENUM('Issued', 'Assigned', 'Picked_up', 'Delivered');
+CREATE TYPE Batch_Status AS ENUM ('Picked_up', 'Delivered');
+CREATE TYPE Shopping_Status AS ENUM('In_store', 'Checked_out');
+
+
+-- load the data
+
+\copy Users(uid, name, role) FROM data/users.csv csv header;
+\copy Issues(iid, initial_date, product, status, priority, uid, message) FROM data/issues.csv csv header;
+\copy Bugs(bid, severity) FROM data/bugs.csv csv header;
+\copy Features(fid, votes) FROM data/features.csv csv header;
+\copy Comments(cid, date, iid, uid, comment) FROM data/comments.csv csv header;
+
